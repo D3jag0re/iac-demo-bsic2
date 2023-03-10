6efa712c5c05e1fbd23fd162977b83251daadfd9
D3jag0re <mbrinkwo@gmail.com>
2023-03-10 11:19:15 -0500

    housekeeping

--- Terraform 1 WinBasic/main.tf
+++ Terraform 1 WinBasic/main.tf
@@ -40,6 +40,18 @@ resource "azurerm_network_security_group" "demonsg" {
   name                = var.sgname
   resource_group_name = azurerm_resource_group.demorg.name
   location            = var.location
+
+      security_rule {
+    name                       = "RDP_Inbound"
+    priority                   = 1011
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 
